09406e8a3aeeecf60933b127aa0b5aed4579c16e
Gopal-6382 <gopalkrishnanto@email.com>
2025-08-25 19:08:12 +0530

    Atoms

--- src/assets/Json/img_project.js
+++ src/assets/Json/img_project.js
@@ -1,12 +1,12 @@
-import bus from '../img/Bus.webp';
-import React1 from '../img/React.jpg';
-import React2 from '../img/React-2.webp';
-import Programming from '../img/Programming.webp';
-import Frontend from '../img/frontend.png';
-import Ecommerce from '../img/Ecommerce.png';
-import Linkden from "../img/linkden.webp"
-import StoryBook from "../img/Storybook.png"
-import Atom from "../img/atom.png"
+import bus from "../img/Bus.webp";
+import React1 from "../img/React.jpg";
+import React2 from "../img/React-2.webp";
+import Programming from "../img/Programming.webp";
+import Frontend from "../img/frontend.png";
+import Ecommerce from "../img/Ecommerce.png";
+import Linkden from "../img/linkden.webp";
+import StoryBook from "../img/Storybook.png";
+import Atom from "../img/Atom.png";
 const imgProject = {
   bus,
   React1,
@@ -15,8 +15,8 @@ const imgProject = {
   Frontend,
   Ecommerce,
   Linkden,
-StoryBook,
-Atom
+  StoryBook,
+  Atom,
 };
 
 export default imgProject;
